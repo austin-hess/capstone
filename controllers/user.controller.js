441f797bb1ada6e8da67a19252ebc7bb688d26d9
Austin Hess <amhess714@gmail.com>
2019-04-17 16:52:31 -0500

    Created profile page and route to it

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,43 +1,22 @@
 const User = require ("../models/user.model");
 
-exports.user_create = function (req, res) {
-    let user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        firstName: req.body.fName,
-        lastName: req.body.lName
-    });
+module.exports = {
 
-    user.save(function (err) {
-        if (err) {
-            return next(err);
-        }
-        res.send('User created successfully');
-    })
-};
+    get_user_profile: function (req, res) {
+        res.render('pages/profile', {user : req.user});
+    },
 
-exports.get_user = function (req, res) {
-    User.findById(req.params.id, function (err, user) {
-        if (err) return next(err);
-        res.send(user);
-    })
-};
+    update_user: function (req, res) {
+        User.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, user) {
+            if (err) return next(err);
+            res.send('User updated');
+        });
+    },
 
-exports.update_user = function (req, res) {
-    User.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, user) {
-        if (err) return next(err);
-        res.send('User updated');
-    });
+    delete_user: function (req, res) {
+        User.findByIdAndRemove(req.params.id, function(err) {
+            if (err) return next(err);
+            res.send('Deleted user succesfully');
+        });
+    }
 }
-
-exports.delete_user = function (req, res) {
-    User.findByIdAndRemove(req.params.id, function(err) {
-        if (err) return next(err);
-        res.send('Deleted user succesfully');
-    });
-}
-
-exports.test = function(req, res) {
-    res.send('Greetings from the Test controller');
-};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth'),
+      isLoggedIn = auth.isLoggedIn;
+
 // Require the controllers
 const user_controller = require('../controllers/user.controller');
-
-router.get('/test', user_controller.test);
-
-router.post('/create', user_controller.user_create);
-
-router.get('/:id', user_controller.get_user);
 
 router.put('/:id/update', user_controller.update_user);
 
 router.delete('/:id/delete', user_controller.delete_user);
+
+router.get('/profile', user_controller.get_user_profile);
 
 module.exports = router;

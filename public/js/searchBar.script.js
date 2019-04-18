36229dce70dd25c6b34bf25604c27f90430894bd
Austin Hess <amhess714@gmail.com>
2019-04-17 23:28:42 -0500

    Started searchBar.script

---
+++ public/js/searchBar.script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+
+    // get the search bar DOM element
+    var searchForm = document.querySelector("#movieSearchForm")
+    var searchBar = document.querySelector("#movieSearchTerm");
+
+}

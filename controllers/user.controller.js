bbb0a7e4671a6430a15ff1c578feee27c46ab524
Austin Hess <amhess714@gmail.com>
2019-04-21 16:04:11 -0500

    Updated manifest

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,35 +5,7 @@ const User                  = require ("../models/user.model"),
 module.exports = {
 
     get_user_profile: function (req, res) {
-
-        var userId = req.user._id;
-        request({
-            method: 'POST',
-            uri: 'https://ibcf-service.ml/prediction',
-            body: {
-                user_id: userId
-            },
-            json: true
-        }, function(err, response, body) {
-            console.log(body);
-            if (err) res.send(err);
-            var recommendations = [];
-            var idlist = [];
-            body.forEach(function(rec) {
-                idlist.push(mongoose.Types.ObjectId(rec.movieId));
-            });
-            Movie.find({'_id': { $in: idlist }}, function(err, movies) {
-                if (err) return res.send(err);
-                movies.forEach(function(item) {
-                    item._id = rec.movieId;
-                    item.title = movie.title;
-                    item.year = movie.year;
-                    item.rating = rec.ratingPred;
-                    recommendations.push(item);
-                })
-            });
-            res.render('pages/profile', {user : req.user, recommendations: recommendations});
-        });
+        
     },
 
     update_user: function (req, res) {
